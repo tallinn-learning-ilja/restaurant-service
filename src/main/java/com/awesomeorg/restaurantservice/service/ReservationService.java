0708f30c0377a17649b0bb5fc747f8a5bbbea6f0
ilniko <ilja.nikolski@hotmail.com>
2023-12-12 20:29:06 +0200

    injected repositories

--- src/main/java/com/awesomeorg/restaurantservice/service/ReservationService.java
+++ src/main/java/com/awesomeorg/restaurantservice/service/ReservationService.java
@@ -1,7 +1,12 @@
 package com.awesomeorg.restaurantservice.service;
 
+import com.awesomeorg.restaurantservice.repository.ReservationRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ReservationService {
+
+    private final ReservationRepository reservationRepository;
 }
