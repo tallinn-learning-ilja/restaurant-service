4b6b992b4aa1a0c7aa6a9f3454a15613c1bf6499
ilniko <ilja.nikolski@hotmail.com>
2023-12-12 20:20:19 +0200

    added controllers for all entities

---
+++ src/main/java/com/awesomeorg/restaurantservice/controller/InternalReservationController.java
@@ -0,0 +1,9 @@
+package com.awesomeorg.restaurantservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/internal/reservations")
+public class InternalReservationController {
+}
