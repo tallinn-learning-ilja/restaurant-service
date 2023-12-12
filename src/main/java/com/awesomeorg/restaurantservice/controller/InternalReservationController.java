06facbd2d47a0efee950c47dd016513c5d882b04
ilniko <ilja.nikolski@hotmail.com>
2023-12-12 20:24:21 +0200

    inject services to controllers

--- src/main/java/com/awesomeorg/restaurantservice/controller/InternalReservationController.java
+++ src/main/java/com/awesomeorg/restaurantservice/controller/InternalReservationController.java
@@ -1,9 +1,14 @@
 package com.awesomeorg.restaurantservice.controller;
 
+import com.awesomeorg.restaurantservice.service.ReservationService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/internal/reservations")
+@RequiredArgsConstructor
 public class InternalReservationController {
+
+    private final ReservationService reservationService;
 }
--- src/main/java/com/awesomeorg/restaurantservice/controller/ReservationController.java
+++ src/main/java/com/awesomeorg/restaurantservice/controller/ReservationController.java
@@ -1,9 +1,14 @@
 package com.awesomeorg.restaurantservice.controller;
 
+import com.awesomeorg.restaurantservice.service.ReservationService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/reservations")
+@RequiredArgsConstructor
 public class ReservationController {
+
+    private final ReservationService reservationService;
 }
